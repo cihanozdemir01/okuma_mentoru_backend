c56ba4b4ad15910fa5c17fc9cd14816546535017
Cihan Özdemir <ozdemircihan@gmail.com>
2025-09-14 01:31:42 +0300

    feat: ISBN ile kitap arama API endpointi eklendi

--- reading/urls.py
+++ reading/urls.py
@@ -1,34 +1,34 @@
-    from django.urls import path
-    from .views import (
-        KitapListCreateAPIView, 
-        KitapDetailAPIView, 
-        NotListCreateAPIView,
-        NoteListAPIView,
-        KategoriListAPIView,
-        AuthorListAPIView,
-        HeatmapAPIView,
-        SummaryAPIView,
-        CharacterChatAPIView
-        FindBookAPIView  
-    )
+from django.urls import path
+from .views import (
+    KitapListCreateAPIView, 
+    KitapDetailAPIView, 
+    NotListCreateAPIView,
+    NoteListAPIView,
+    KategoriListAPIView,
+    AuthorListAPIView,
+    HeatmapAPIView,
+    SummaryAPIView,
+    CharacterChatAPIView,
+    FindBookAPIView  
+)
 
-    urlpatterns = [
-        # Kitap & Not URL'leri
-        path('kitaplar/', KitapListCreateAPIView.as_view(), name='kitap-list-create'),
-        path('kitaplar/<int:pk>/', KitapDetailAPIView.as_view(), name='kitap-detail'),
-        path('kitaplar/<int:kitap_pk>/notlar/', NotListCreateAPIView.as_view(), name='not-list-create'),
-        path('notes/', NoteListAPIView.as_view(), name='note-list-all'),
+urlpatterns = [
+    # Kitap & Not URL'leri
+    path('kitaplar/', KitapListCreateAPIView.as_view(), name='kitap-list-create'),
+    path('kitaplar/<int:pk>/', KitapDetailAPIView.as_view(), name='kitap-detail'),
+    path('kitaplar/<int:kitap_pk>/notlar/', NotListCreateAPIView.as_view(), name='not-list-create'),
+    path('notes/', NoteListAPIView.as_view(), name='note-list-all'),
 
-        # ISBN ile kitap bulma yolu
-        path('find-book/', FindBookAPIView.as_view(), name='find-book'),
+    # ISBN ile kitap bulma yolu
+    path('find-book/', FindBookAPIView.as_view(), name='find-book'),
 
-        # Filtreleme için yardımcı URL'ler
-        path('kategoriler/', KategoriListAPIView.as_view(), name='kategori-list'),
-        path('authors/', AuthorListAPIView.as_view(), name='author-list'),
+    # Filtreleme için yardımcı URL'ler
+    path('kategoriler/', KategoriListAPIView.as_view(), name='kategori-list'),
+    path('authors/', AuthorListAPIView.as_view(), name='author-list'),
 
-        # İstatistik URL'leri
-        path('stats/heatmap/', HeatmapAPIView.as_view(), name='heatmap-data'),
-        path('stats/summary/', SummaryAPIView.as_view(), name='summary-api'),
+    # İstatistik URL'leri
+    path('stats/heatmap/', HeatmapAPIView.as_view(), name='heatmap-data'),
+    path('stats/summary/', SummaryAPIView.as_view(), name='summary-api'),
 
-        path('character-chat/', CharacterChatAPIView.as_view(), name='character-chat'),
-    ]
+    path('character-chat/', CharacterChatAPIView.as_view(), name='character-chat'),
+]    
